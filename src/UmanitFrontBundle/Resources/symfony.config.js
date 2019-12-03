e410d2399f2631b4e79b2e174f05e30e893077ca
rbremaud <rbremaud@umanit.fr>
2019-12-03 15:00:48 +0100

    webpack config activation de purge css uniquement si en mode production

--- src/UmanitFrontBundle/Resources/symfony.config.js
+++ src/UmanitFrontBundle/Resources/symfony.config.js
@@ -4,23 +4,25 @@ let path = require('path');
 ###< imports ###
 
 ###> config ###
-Encore.addPlugin(new PurgecssPlugin({
-    paths: glob.sync([
-            path.join(__dirname, 'templates/**/*.html.twig'),
-            'vendor/umanit/front-bundle/src/UmanitFrontBundle/Resources/views/style_guide/**/*.html.twig',
-            assetPath + '/js/**/*.js'
-        ]
-    ),
-    extractors: [
-        {
-            extractor: class {
-                static extract(content) {
-                    return content.match(/[A-z0-9-:\/]+/g) || []
-                }
-            },
-            extensions: ['twig','js'],
-        }
-    ],
-    whitelistPatterns: [/slick/,/select2/,/link--i/]
-}));
+if(Encore.isProduction())
+{
+    Encore.addPlugin(new PurgecssPlugin({
+        paths: glob.sync([
+                path.join(__dirname, 'templates/**/*.html.twig'),
+                assetPath + '/js/**/*.js'
+            ]
+        ),
+        extractors: [
+            {
+                extractor: class {
+                    static extract(content) {
+                        return content.match(/[A-z0-9-:\/]+/g) || []
+                    }
+                },
+                extensions: ['twig', 'js'],
+            }
+        ],
+        whitelistPatterns: [/slick/, /select2/, /link--i/]
+    }))
+}
 ###< config ###
